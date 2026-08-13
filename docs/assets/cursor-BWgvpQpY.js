240cee6fae8300fddb7d42e918e23db488396475
Danialzz <dani.zaman8@gmail.com>
2026-08-13 23:40:45 +0330

    Make premium control styling unmistakable on the live UI

---
+++ docs/assets/cursor-BWgvpQpY.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-Cg03bQTr.js";import"./index-D-MXP82U.js";import"./index.es-BSab6QRM.js";import"./events-DQ172AOg.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
